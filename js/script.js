0774a95346696968bcff06dc775c0f5b6ffb96a9
Aznhelika Khamaza <anzhelikakhamaza@gmail.com>
2024-09-27 10:31:01 -0500

    Updated option functionality

--- js/script.js
+++ js/script.js
@@ -1,38 +1,67 @@
-const pizzaType = document.querySelector('.pizzaType');
-const pizzaSize = document.querySelector('.pizzaSize');
-const extraIngredients = document.querySelector('.add_extras');
-const orderButton = document.querySelector('.order_button');
-const displayTotalPrice = document.querySelector('.totalPrice');
+const pizzaPhoto = document.querySelectorAll(".pizza_option");
+const pizzaType = document.querySelector(".pizza_type");
+const pizzaSize = document.querySelector(".pizza_size");
+const extraIngredients = document.querySelector(".add_extras");
+const orderButton = document.querySelector(".order_button");
+const displayTotalPrice = document.querySelector(".total_price");
+
+const myForm = document.getElementById("myForm");
+const mySelect = document.getElementById("mySelect");
+
 const checkboxes = extraIngredients.querySelectorAll('input[type="checkbox"]');
+
+pizzaType.addEventListener("change", updatePizzaType);
+pizzaSize.addEventListener("change", updatePizzaSize);
+extraIngredients.addEventListener("change", addExtras);
+orderButton.addEventListener("click", makeOrder);
 
 let selectedPizzaType = 0;
 let selectedPizzaSize = 0;
 let selectedIngredients = 0;
 
 function updatePizzaType() {
-	selectedPizzaType = parseFloat(pizzaType.value);
+  selectedPizzaType = parseFloat(pizzaType.value);
 }
 function updatePizzaSize() {
-	selectedPizzaSize = parseFloat(pizzaSize.value);
+  selectedPizzaSize = parseFloat(pizzaSize.value);
 }
 
-pizzaType.addEventListener('change', updatePizzaType);
-pizzaSize.addEventListener('change', updatePizzaSize);
+function addExtras() {
+  let totalIngredients = 0;
 
-extraIngredients.addEventListener('change', function () {
-	let totalIngredients = 0;
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      totalIngredients += parseFloat(checkbox.value);
+    }
+  });
 
-	checkboxes.forEach(checkbox => {
-		if (checkbox.checked) {
-			totalIngredients += parseFloat(checkbox.value);
-		}
-	});
+  selectedIngredients = totalIngredients;
+}
 
-	selectedIngredients = totalIngredients;
+function makeOrder() {
+  const calculateTotalCost =
+    selectedPizzaType + selectedPizzaSize + selectedIngredients;
+
+  displayTotalPrice.textContent = "Total: $" + calculateTotalCost;
+}
+
+pizzaPhoto.forEach((button) => {
+  button.addEventListener("click", imgClick);
 });
 
-orderButton.addEventListener('click', function () {
-	const calculateTotalCost = selectedPizzaType + selectedPizzaSize + selectedIngredients;
+function imgClick() {
+  const pizzaTypeValue = this.value;
 
-	displayTotalPrice.textContent = 'Total: $' + calculateTotalCost;
-});
+  if (pizzaTypeValue === "margarita") {
+    selectedPizzaType = parseFloat(mySelect.options[1].value);
+    mySelect.value = "20";
+  } else if (pizzaTypeValue === "pepperoni") {
+    selectedPizzaType = parseFloat(mySelect.options[2].value);
+    mySelect.value = "23";
+  } else if (pizzaTypeValue === "hawaiian") {
+    selectedPizzaType = parseFloat(mySelect.options[3].value);
+    mySelect.value = "18";
+  }
+
+  updatePizzaType();
+}
