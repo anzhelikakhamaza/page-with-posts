7f357e160cfd0cc061543170e87d4ce6c1ede406
Aznhelika Khamaza <anzhelikakhamaza@gmail.com>
2024-10-09 16:22:25 -0500

    Add Delete button and drawn To Dos on the page

--- js/script.js
+++ js/script.js
@@ -2,37 +2,70 @@ const titleInput = document.getElementById("taskInputTitle");
 const descriptionInput = document.getElementById("taskInputDescription");
 const addTaskButton = document.getElementById("addTaskBtn");
 
+const deleteButtonContainer = document.getElementById("button-container");
 const taskContainer = document.getElementById("taskList");
-
-titleInput.addEventListener("change", saveTitle);
-descriptionInput.addEventListener("change", saveDescription);
 
 addTaskButton.addEventListener("click", addTask);
 
-function saveDescription() {
-  const descriptionValue = descriptionInput.value;
-  localStorage.setItem("savedDescription", descriptionValue);
-}
-
-function saveTitle() {
-  const titleValue = titleInput.value;
-  localStorage.setItem("savedTitle", titleValue);
-}
+let toDos = [];
 
 function addTask() {
-  const titleValue = titleInput.value;
-  const descriptionValue = descriptionInput.value;
+  const newToDo = {
+    title: titleInput.value,
+    description: descriptionInput.value,
+  };
 
-  if (titleValue && descriptionValue) {
-    const taskTitleElement = document.createElement("div");
-    taskTitleElement.classList.add("insertedTitle");
-    taskTitleElement.innerHTML = titleValue;
+  toDos.push(newToDo);
 
-    const taskDescriptionElement = document.createElement("div");
-    taskDescriptionElement.classList.add("insertedDescription");
-    taskDescriptionElement.innerHTML = descriptionValue;
+  taskContainer.replaceChildren();
 
-    taskContainer.appendChild(taskTitleElement);
-    taskContainer.appendChild(taskDescriptionElement);
+  drawToDoes();
+  setToDoesToLocalStorage();
+  deleteToDoes();
+}
+
+function drawToDoes() {
+  for (let i = 0; i < toDos.length; i++) {
+    const titleWrapper = document.createElement("div");
+    titleWrapper.classList.add("insertedTitle");
+    titleWrapper.innerHTML = toDos[i].title;
+
+    const descriptionWrapper = document.createElement("div");
+    descriptionWrapper.classList.add("insertedDescription");
+    descriptionWrapper.innerHTML = toDos[i].description;
+
+    const deleteButtonWrapper = document.createElement("button");
+    deleteButtonWrapper.classList.add("deleteTask");
+    deleteButtonWrapper.innerText = "Delete";
+
+    deleteButtonWrapper.addEventListener("click", function () {
+      deleteTask(i);
+    });
+
+    deleteButtonContainer.appendChild(deleteButtonWrapper);
+    taskContainer.appendChild(titleWrapper);
+    taskContainer.appendChild(descriptionWrapper);
   }
 }
+
+function deleteTask(index) {
+  toDos.splice(index, 1);
+
+  addTask();
+}
+
+function setToDoesToLocalStorage() {
+  localStorage.setItem("toDos", JSON.stringify(toDos));
+}
+
+function getToDoesToLocalStorage() {
+  let enteredData = localStorage.getItem("toDos");
+
+  if (enteredData) {
+    toDos = JSON.parse(enteredData);
+    taskContainer.replaceChildren();
+    drawToDoes();
+  }
+}
+
+window.onload = getToDoesToLocalStorage;
